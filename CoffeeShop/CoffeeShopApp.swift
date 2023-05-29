0171a6201a1176af6027304c1e54d6bdddcb8c4b
Rizky Rachmat <rizky.rachmat@gopay.co.id>
2023-05-29 07:17:30 +0700

    Initial Commit

---
+++ CoffeeShop/CoffeeShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeShopApp.swift
+//  CoffeeShop
+//
+//  Created by Rizky Rachmat on 29/05/23.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
